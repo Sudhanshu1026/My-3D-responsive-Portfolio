1aa306934f59bdb9b9b0c2fc5cdb45c09e876479
Sudhanshu Singh <sudhanshusingh1026@gmail.com>
2025-11-07 21:20:47 +0530

    completed

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Programmer", "Problem Solver"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+});
